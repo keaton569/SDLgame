7d72636c2c071da67879ba3aa7c4db55414871db
Keaton Benning <keatonguitar123@gmail.com>
2019-04-09 21:20:18 -0400

    assets folder

--- 2d_GameEngine/2d_GameEngine/main.cpp
+++ 2d_GameEngine/2d_GameEngine/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, const char * argv[]) {
 	game = new Game();
 
 
-	game->init("engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	game->init("engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, true);
 
 	while (game->isRunning)
 	{
