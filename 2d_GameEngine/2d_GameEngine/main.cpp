54fec4e4c983dd0b6a1dad13f34d72b614d20ed9
Keaton Benning <keatonguitar123@gmail.com>
2019-04-09 21:52:14 -0400

    frame rate setup

--- 2d_GameEngine/2d_GameEngine/main.cpp
+++ 2d_GameEngine/2d_GameEngine/main.cpp
@@ -7,6 +7,13 @@ Game* game = nullptr;
 
 int main(int argc, const char * argv[]) {
 	
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	game = new Game();
 
 
@@ -14,9 +21,21 @@ int main(int argc, const char * argv[]) {
 
 	while (game->isRunning)
 	{
+
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+
 	}
 
 
